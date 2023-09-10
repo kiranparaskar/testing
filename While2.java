553b8cef46c054ca744cb55f3d06aa18c54dffc6
kiranparaskar <kiranparaskar1502@gmail.com>
2023-09-10 11:44:54 +0530

    push

---
+++ While2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class While2{
+	
+	public static void main(String[] args){
+		int i=10;
+		while (i>=10){
+			System.out.println("love you");
+			i=i-9;
+		}
+	}
+}
