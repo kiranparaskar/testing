d1bd090236c871e11797e86c8f3160234687539e
kiranparaskar <kiranparaskar1502@gmail.com>
2023-10-26 17:17:49 +0530

    Msg

---
+++ Homework.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Homework{
+	
+	public static void main(String[] args){
+		char ch ='o';
+		if(ch=='a' || ch=='e' || ch=='i'|| ch=='o'|| ch=='u'){
+			System.out.println(ch+" is vowel");
+		
+		} else{
+			System.out.println(ch+" is not a vowel");
+		}
+	}
+}
